d1d03fe5fb43834b655fd65dbfda201970b1f54f
Jagadeesh <jagadeesh.jkp@gmail.com>
2019-04-18 19:52:26 +0530

    Update makeCall.js

--- makeCall.js
+++ makeCall.js
@@ -11,7 +11,7 @@ module.exports = function(){
     client.calls
         .create({
             url: 'http://demo.twilio.com/docs/voice.xml',
-            to: myPhone,  // Text this number
+            to: myPhone,  // Call this number
             from: TwilioPhone // From a valid Twilio number
         })
         .then(call => console.log(call.sid));
